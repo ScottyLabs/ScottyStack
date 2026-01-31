f193f4370bcda8aba675c4f1c4e972bb9efeb69b
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-01-31 00:24:07 -0500

    chore(init): set up Express server

---
+++ apps/server/src/server.ts
@@ -0,0 +1,64 @@
+// import fs from "node:fs";
+import http from "node:http";
+import process from "node:process";
+// import { toNodeHandler } from "better-auth/node";
+// import { YAML } from "bun";
+import cors, { type CorsOptions } from "cors";
+// import type { ErrorRequestHandler } from "express";
+import express from "express";
+// import swaggerUi, { type JsonObject } from "swagger-ui-express";
+import { env } from "./env.ts";
+// import { auth } from "./lib/auth.ts";
+// import { errorHandler } from "./middleware/errorHandler.ts";
+import { notFoundHandler } from "./middlewares/notFoundHandler.ts";
+
+const app = express();
+app.use(express.json({ limit: "1mb" }));
+
+// Define CORS options
+const corsOptions: CorsOptions = {
+  origin: env.ALLOWED_ORIGINS_REGEX?.split(",").map(
+    (origin) => new RegExp(origin),
+  ),
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Create HTTP server with Express app attached
+const server = http.createServer(app);
+
+// Setup Authentication
+// Setup Authentication: https://www.better-auth.com/docs/integrations/express
+// app.all("/api/auth/*splat", toNodeHandler(auth));
+
+// Swagger and OpenAPI JSON
+// const swaggerYaml = fs.readFileSync("./build/swagger.yaml", "utf8");
+// const swaggerJson = YAML.parse(swaggerYaml) as JsonObject;
+// app.use(
+//   "/swagger",
+//   express.static("./node_modules/swagger-ui-dist", { index: false }),
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerJson),
+// );
+// app.get("/openapi", (_req, res) => {
+//   res.status(200).send(swaggerJson);
+// });
+
+// Routes
+// RegisterRoutes(app);
+app.get("/", (_req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+// Error Handling and Not Found Handlers
+// app.use(errorHandler as ErrorRequestHandler);
+app.use(notFoundHandler);
+
+const port = env.SERVER_PORT;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+process.on("SIGINT", async () => {
+  process.exit();
+});
