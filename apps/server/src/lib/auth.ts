dab4039a86bed44fe08976c3a16c70b104035788
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-07 20:45:18 -0500

    feat(auth): make id in user table the AndrewID

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import type { Session, User } from "better-auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { customSession, genericOAuth, keycloak } from "better-auth/plugins";
+import { customSession, genericOAuth } from "better-auth/plugins";
 import jwt from "jsonwebtoken";
 import { db } from "../db/index.ts";
 import * as schema from "../db/schema/index.ts";
@@ -24,21 +24,63 @@ export const auth = betterAuth({
   baseURL: env.SERVER_URL,
   trustedOrigins: [env.BETTER_AUTH_URL],
 
+  // Add full email as an additional field to the user object
+  // in order to consitently retrieve the Andrew ID
+  user: {
+    additionalFields: {
+      fullEmail: {
+        type: "string",
+        required: true,
+      },
+    },
+  },
+
+  // Change the user id to Andrew ID when creating a new user
+  databaseHooks: {
+    user: {
+      create: {
+        before: async (user) => {
+          return {
+            data: {
+              ...user,
+              // @ts-expect-error - fullEmail is an additional field
+              id: user["fullEmail"].split("@")[0],
+            },
+          };
+        },
+      },
+    },
+  },
+
   plugins: [
     // https://www.better-auth.com/docs/plugins/generic-oauth#pre-configured-provider-helpers
     genericOAuth({
       config: [
-        keycloak({
+        {
+          providerId: "keycloak",
           clientId: env.AUTH_CLIENT_ID,
           clientSecret: env.AUTH_CLIENT_SECRET,
-          issuer: env.AUTH_ISSUER,
+          discoveryUrl: `${env.AUTH_ISSUER}/.well-known/openid-configuration`,
           redirectURI: `${env.SERVER_URL}/api/auth/oauth2/callback/keycloak`,
           scopes: ["openid", "email", "profile", "offline_access"],
-        }),
+          mapProfileToUser: (profile) => {
+            return {
+              id: profile["sub"],
+              name: profile["name"],
+              email: profile["email"],
+              emailVerified: profile["email_verified"],
+              image: null,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              fullEmail: profile["full_email"],
+            };
+          },
+        },
       ],
     }),
 
-    // https://www.better-auth.com/docs/concepts/session-management#customizing-session-response
+    // Add groups to the session so it can used easily in the frontend via `useSession` hook.
+    // Reference: https://www.better-auth.com/docs/concepts/session-management#customizing-session-response
     customSession(async ({ user, session }, ctx): Promise<Auth> => {
       const customSessionObject: Auth = { session, user };
 
