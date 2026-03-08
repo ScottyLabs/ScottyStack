a30ab895dc0fe48e3ba7ae48bf0cd1081e1e0895
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-03-08 19:48:31 -0400

    feat(server): access env via process in Sentry
    So we don't need to also copy over the env file lol

--- apps/server/src/instrument.ts
+++ apps/server/src/instrument.ts
@@ -1,6 +1,5 @@
 import * as Sentry from "@sentry/bun";
-import { env } from "./env";
 
 // Reference: https://docs.sentry.io/platforms/javascript/guides/bun/
-const result = Sentry.init({ dsn: env.SENTRY_DSN });
+const result = Sentry.init({ dsn: process.env["SENTRY_DSN"] });
 console.log("Sentry initialized with DSN:", result?.getOptions().dsn);
