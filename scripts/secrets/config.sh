cfd5fa69cfc904d72aa4b479589ea368af19ea43
Yuxiang-Huang <yuxiangh142857@gmail.com>
2026-08-08 16:12:37 -0400

    refactor(secrets): manage secrets with SecretSpec
    No longer need to maintain the bash scripts "prev me" wrote, yay!

--- scripts/secrets/config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export BAO_ADDR="https://bao.scottylabs.org"
-export BAO_MOUNT="labrador"
-export PROJECT="scottystack"
-export APPS="web server"
-export ENVS="local dev staging prod"
-export EMPTY_JSON='{"data":{"data":{}}}'
